6279ea499b7bca12fa846e75e3054ff851c550be
deviding <sankakujyougi39@gmail.com>
2023-09-18 22:17:29 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// パスワードを作成する関数
+function createPassword() {
+  // 桁数の要素を取得
+  const numberElement = document.getElementById("number");
+
+  // ランダムなパスワードを作成
+  let result = "";
+  do {
+    result = "";
+    for(let i = 0; i < numberElement.value; i++) {
+      result += getOneRandomStr(result);
+    }
+  } while (!isOkPass(result));
+
+  // 作成したパスワードを表示
+  const passDiv = document.getElementById("pass");
+  passDiv.innerText = result;
+}
+
+// 
+function getOneRandomStr(result) {
+  // 使用文字の定義
+  const passStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&=~/*-+?_";
+
+  // 含まれていない文字を一文字取得
+  let oneStr = "";
+  do {
+    oneStr = passStr.charAt(Math.floor(Math.random() * passStr.length));
+  } while (result.indexOf(oneStr) >= 0);
+
+  return oneStr;
+}
+
+// パスワードがOKかを判定する関数
+function isOkPass(pass) {
+  const existNumber = pass.search(/[0-9]/g);
+  const existSmall  = pass.search(/[a-z]/g);
+  const existBig    = pass.search(/[A-Z]/g);
+  const existSymbol = pass.search(/[!#\$%&=~/\*\-\+\?_]/g);
+
+  if ((existNumber >= 0) && (existSmall >= 0) && (existBig >= 0) && (existSymbol >= 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
